09bce137513394d3e27faad303f61b6b4d2c23cc
Abhisheikh-G <abhisheikhg@gmail.com>
2020-12-23 16:42:48 -0500

    Add productDeleteReducer/action/constants. Add dispatch on ProductListScreen

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,16 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 import {
+  deleteProduct,
   getProducts,
   getProductById,
-} from '../controllers/productController.js'
+} from "../controllers/productController.js";
+import { isAdmin, protect } from "../middleware/authMiddleware.js";
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router.route("/").get(getProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, isAdmin, deleteProduct);
 
-export default router
+export default router;
---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,110 @@
+import React, { useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listProducts, deleteProduct } from "../actions/productActions";
+
+function ProductListScreen({ history }) {
+  const dispatch = useDispatch();
+  const productList = useSelector((st) => st.productList);
+  const { loading, products, error } = productList;
+
+  const userLogin = useSelector((st) => st.userLogin);
+  const { userInfo } = userLogin;
+
+  const productDelete = useSelector((st) => st.productDelete);
+  const {
+    loading: productDelLoading,
+    success: productDelSuccess,
+    message: productDelMessage,
+    error: productDelError,
+  } = productDelete;
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listProducts());
+    } else {
+      history.push("/login");
+    }
+  }, [dispatch, history, userInfo, productDelete]);
+
+  const handleDeleteProduct = (product) => {
+    if (
+      window.confirm(`Are you sure you want to delete product: ${product.name}`)
+    ) {
+      dispatch(deleteProduct(product._id));
+    }
+  };
+
+  const handleCreateProduct = () => {};
+
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-right">
+          <Button className="my-3" onClick={handleCreateProduct}>
+            <i className="fas fa-plus"></i> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {productDelLoading && <Loader />}
+      {productDelError && <Message variant="error">{productDelError}</Message>}
+      {productDelSuccess && (
+        <Message variant="success">{productDelMessage}</Message>
+      )}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+
+                  <td>
+                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                      <Button variant="light" className="btn-sm">
+                        <i className="fas fa-edit"></i>
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => handleDeleteProduct(product)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
+}
+
+export default ProductListScreen;
